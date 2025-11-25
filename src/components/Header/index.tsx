1481ac64f2e7dcc5689df6e29d5e7ff0d1bd2f89
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-11-25 16:54:20 -0300

    implementaçao e atualizacao de pagina

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-icons/fa';
 import Container from './styles';
 import { FaWhatsapp } from 'react-icons/fa';
+import Chat from '../../assets/images/Chat.png';
 
 export default function Header() {
   const [active, setMode] = useState(false);
@@ -42,7 +43,7 @@ export default function Header() {
         <nav>
           <div id="logo">
             <a href="/">
-              <img src="/assets/images/Chat.png" alt="Logo da Segplan" />
+              <img src={Chat} alt="Logo da Segplan" />
             </a>
           </div>
           <div className={active ? 'nav-menu  active' : 'nav-menu '}>
